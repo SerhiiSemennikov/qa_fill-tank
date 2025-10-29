d417e2eeb1caaf448d44c6e72581e5ae4f17ac37
SerhiiSemennikov <semennik@hotmail.com>
2025-10-29 21:38:32 +0100

    copy1

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,143 @@
-'use strict';
+"use strict";
 
-describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+describe("fillTank", () => {
+  const { fillTank } = require("./fillTank");
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    customer = {
+      money: 3000, // customer account balance
+      vehicle: {
+        maxTankCapacity: 40, // fuel tank volume
+        fuelRemains: 8, // Remaining fuel in the tank
+      },
+    };
   });
 
-  // write tests here
+  it("fuelPrice 1 baks / 1 liter and amount 10 liters", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 1, 10);
+
+    expect(customer.money).toBe(2990);
+    expect(customer.vehicle.fuelRemains).toBe(18);
+
+    const result = fillTank(customer, 1, 10);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
+
+  it("fuelPrice 1.11 baks / 1 liter and amount is not given", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 1.11);
+
+    expect(customer.money).toBe(2964.48);
+    expect(customer.vehicle.fuelRemains).toBe(40);
+
+    const result = fillTank(customer, 1.11);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
+
+  it("fuelPrice 0.99 baks / 1 liter and amount > tank volume", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 0.99, 100);
+
+    expect(customer.money).toBe(2968.32);
+    expect(customer.vehicle.fuelRemains).toBe(40);
+
+    const result = fillTank(customer, 0.99, 100);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
+
+  it("fuelPrice 100 baks / 1 liter and full tank ", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 100);
+
+    expect(customer.money).toBe(0);
+
+    expect(customer.vehicle.fuelRemains).toBe(38);
+
+    const result = fillTank(customer, 100);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
+
+  it("decimal fuelPrice and amount ", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 1.23456789, 12.3456789);
+
+    expect(customer.money).toBe(2984.81); // to the nearest hundredth part
+
+    expect(customer.vehicle.fuelRemains).toBe(20.3); // to the tenth part
+
+    const result = fillTank(customer, 100);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
+
+  it("not tank if amount < 2 ", () => {
+    expect(customer).toHaveProperty("money");
+    expect(customer).toHaveProperty("vehicle");
+    expect(customer).toHaveProperty("vehicle.maxTankCapacity");
+    expect(customer).toHaveProperty("vehicle.fuelRemains");
+
+    expect(customer.money).toBe(3000);
+    expect(customer.vehicle.maxTankCapacity).toBe(40);
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    fillTank(customer, 1, 1.99);
+
+    expect(customer.money).toBe(3000);
+
+    expect(customer.vehicle.fuelRemains).toBe(8);
+
+    const result = fillTank(customer, 100);
+    expect(result).toBeUndefined();
+
+    expect(isObject(customer)).toBeTruthy();
+  });
 });
+
+function isObject(value) {
+  return value instanceof Object && value !== null && !Array.isArray(value);
+}
